115f0563730fbbc60fbf013defbdca3c51e93861
wryther <rytherwy@mail.uc.edu>
2023-02-05 23:17:55 -0500

    Initial dto code commit

--- app/src/main/java/com/taskruler/dto/Task.kt
+++ app/src/main/java/com/taskruler/dto/Task.kt
@@ -1,4 +1,15 @@
 package com.taskruler.dto
 
-class Task {
+import androidx.room.Entity
+
+@Entity(tableName="tasks")
+data class Task {
 }
+
+//Example
+//@Entity(tableName="plants")
+//data class Plant(@SerializedName("genus") var genus : String, var species : String, var common : String, var cultivar:String ="", @PrimaryKey var id : Int = 0 ) {
+//    override fun toString(): String {
+//        return common
+//    }
+//}
