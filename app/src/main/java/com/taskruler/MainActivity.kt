3ba343a7554a7894cd2f243c62afa7d3ab4529aa
thoma4kw <thoma4kw@mail.uc.edu>
2023-02-24 15:42:04 -0500

    First text field

--- app/src/main/java/com/taskruler/MainActivity.kt
+++ app/src/main/java/com/taskruler/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.taskruler.ui.theme.TaskRulerTheme
 
@@ -28,7 +30,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    var activityName by remember { mutableStateOf("")}
+    TextField (
+        value = activityName,
+        onValueChange = {activityName = it},
+        label = { Text(stringResource(R.string.activtyName))}
+    )
 }
 
 @Preview(showBackground = true)
