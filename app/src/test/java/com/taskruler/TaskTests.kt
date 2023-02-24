8829dc0e86eee50203cb472223c2244d831a4979
Aaron Kitelinger <kiteliad@mail.uc.edu>
2023-02-23 23:11:54 -0500

    Added first Unit Test

--- app/src/test/java/com/taskruler/TaskTests.kt
+++ app/src/test/java/com/taskruler/TaskTests.kt
@@ -1,7 +1,11 @@
 package com.taskruler
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.taskruler.dto.Task
+import com.taskruler.service.TaskService
+import junit.framework.TestCase.assertNotNull
 import org.junit.Rule
+import org.junit.Test
 import org.junit.rules.TestRule
 
 class TaskTests {
@@ -9,7 +13,60 @@ class TaskTests {
     var rule: TestRule = InstantTaskExecutorRule()
 
 
+    lateinit var mockTaskService : TaskService
+    val laundrytask = Task(0,"Do Laundry", isCompleted = false)
+
+    var tasks : List<Task>? = ArrayList<Task>()
+
 
     //Test File
+    @Test
+    fun `Add a task to a Database`() {
+        givenTaskDatabaseExists()
+        whenDoLaundryisEntered()
+        DoLaundryIsAddedToDatabase()
+
+
+    }
+
+    private fun givenTaskDatabaseExists() {
+        assertNotNull(tasks)
+    }
+
+    private fun whenDoLaundryisEntered() {
+        assertNotNull(laundrytask)
+    }
+
+    private fun DoLaundryIsAddedToDatabase() {
+        var addTask = ArrayList<Task>()
+
+        addTask.add(laundrytask)
+
+        tasks = addTask
+
+    }
+
+    @Test
+    fun `Delete task from database`() {
+        givenTaskDatabaseExists()
+        whenDeleteButtonisClicked()
+        deleteTask()
+
+    }
+
+    private fun deleteTask() {
+      var removeTask = tasks
+
+
+
+
+    }
+
+    private fun whenDeleteButtonisClicked() {
+
+    }
+
 
 }
+
+
