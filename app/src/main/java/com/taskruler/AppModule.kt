2d537bc237b9c013fea473c17dec4c84febe3be8
wryther <rytherwy@mail.uc.edu>
2023-02-27 22:27:46 -0500

    Adding ITaskService/TaskService to Koin AppModule

--- app/src/main/java/com/taskruler/AppModule.kt
+++ app/src/main/java/com/taskruler/AppModule.kt
@@ -1,9 +1,12 @@
 package com.taskruler
 
+import com.taskruler.service.ITaskService
+import com.taskruler.service.TaskService
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 @JvmField
 val appModule = module {
     viewModel { MainViewModel(get()) }
+    single<ITaskService> { TaskService() }
 }
