e9ec2cee35ebf987374b61d552ba2f5485ab3bb3
lizbissinger <105609716+lizbissinger@users.noreply.github.com>
2023-02-28 23:15:25 -0500

    My code review suggestions.

--- app/src/main/java/com/taskruler/RetrofitClientInstance.kt
+++ app/src/main/java/com/taskruler/RetrofitClientInstance.kt
@@ -5,14 +5,14 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClientInstance {
     private var retrofit: Retrofit? = null
-    private val BASE_URL = "https://wrytheruc.github.io/"
+    private const val BASE_URL = "https://wrytheruc.github.io/"
 
     val retrofitInstance : Retrofit?
         get() {
             // has this object been created yet?
             if (retrofit == null) {
                 // create it
-                retrofit = retrofit2.Retrofit.Builder()
+                retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
