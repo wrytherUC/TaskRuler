6d11538267265d2abdde95af968512c0e5c0c40a
thoma4kw <thoma4kw@mail.uc.edu>
2023-03-16 16:36:51 -0400

    Spinner draft

--- app/src/main/java/com/taskruler/MainActivity.kt
+++ app/src/main/java/com/taskruler/MainActivity.kt
@@ -3,22 +3,33 @@ package com.taskruler
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.taskruler.dto.Task
 import com.taskruler.ui.theme.TaskRulerTheme
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.w3c.dom.Text
+import com.google.android.gms.tasks.Task as Task1
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity<activtyTask> : ComponentActivity() {
 
+    private var selectedTask: Task? = null
     private val viewModel: MainViewModel by viewModel<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +48,53 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-@Composable
+
+
+    @Composable
+    fun TaskSpinner (tasks: List<Task>){
+        var activityText by remember { mutableStateOf("") }
+        var expanded by remember { mutableStateOf(false) }
+        Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+            Row(
+                Modifier
+                    .padding(24.dp)
+                    .clickable {
+                        expanded = !expanded
+                    }
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ){
+                Text( text= activityText, fontSize = 18.sp, modifer = Modifier.padding(end= 8.dp))
+                Icon(ImageVector = Icons.Filled.ArrowDropDown, contentDescription = "")
+
+                DropdownMenu(expanded = expanded, onDismissRequest = {expanded = false}){
+                    tasks.forEach{
+                            task -> DropdownMenuItem(onClick = {
+                        expanded= false
+                        activityText = task.toString()
+                        selectedTask = task
+                    }) {
+                        Text(text = task.toString())
+                    }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+    private fun Icon(ImageVector: ImageVector, contentDescription: String) {
+
+    }
+
+    private fun Text(text: String, fontSize: TextUnit, modifer: Modifier) {
+
+    }
+
+    @Composable
 fun Greeting(name: String) {
 
     var activityName by remember { mutableStateOf("") }
@@ -94,4 +151,7 @@ fun DefaultPreview() {
     TaskRulerTheme {
         Greeting("Android")
     }
-}}
+}
+
+
+
