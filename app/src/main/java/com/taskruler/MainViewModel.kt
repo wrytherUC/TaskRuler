fc191dcd50bb9d0e55d7d25a9dcbd3c53293036b
Aaron Kitelinger <kiteliad@mail.uc.edu>
2023-03-16 17:40:26 -0400

    Adding Firebase Logic to MainViewModel.kt

--- app/src/main/java/com/taskruler/MainViewModel.kt
+++ app/src/main/java/com/taskruler/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.taskruler
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.taskruler.dto.Task
 import com.taskruler.service.ITaskService
 import com.taskruler.service.TaskService
@@ -18,8 +21,39 @@ import kotlinx.coroutines.launch
 class MainViewModel(var taskService : ITaskService = TaskService()) : ViewModel() {
 
     var tasks : MutableLiveData<List<Task>> = MutableLiveData<List<Task>>()
+    var spinnerTasks : MutableLiveData<List<Task>> = MutableLiveData<List<Task>>()
+
 
     private lateinit var firestore : FirebaseFirestore
+
+    init{
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+        listenToTasks()
+    }
+
+    private fun listenToTasks(){
+        firestore.collection("tasks").addSnapshotListener{
+            snapshot, e ->
+            if (e != null){
+                Log.w("Listen failed", e)
+                return@addSnapshotListener
+
+            }
+            snapshot?.let{
+                val allTasks = ArrayList<Task>()
+                val documents = snapshot.documents
+                documents.forEach {
+                    val task = it.toObject(Task::class.java)
+                    task?.let {
+                        allTasks.add(it)
+                    }
+                }
+                spinnerTasks.value = allTasks
+
+            }
+        }
+    }
 
     /**
      * @return tasks from TaskService
