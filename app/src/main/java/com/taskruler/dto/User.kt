83cfbf3425ef0f44d980f26f5302ed05707c32f0
wryther <rytherwy@mail.uc.edu>
2023-03-25 15:24:35 -0400

    Added displayName constructor to User

--- app/src/main/java/com/taskruler/dto/User.kt
+++ app/src/main/java/com/taskruler/dto/User.kt
@@ -3,4 +3,4 @@ package com.taskruler.dto
 /**
  * Data class to setup unique users for app usage/authentication/Firebase
  */
-data class User(val uid: String = "")
+data class User(val uid: String = "", var displayName: String? = "")
