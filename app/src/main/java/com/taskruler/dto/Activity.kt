4728db2f567f960f4216a75885562789dbdfa656
wryther <rytherwy@mail.uc.edu>
2023-03-26 00:43:18 -0400

    Changed activityId back to int

--- app/src/main/java/com/taskruler/dto/Activity.kt
+++ app/src/main/java/com/taskruler/dto/Activity.kt
@@ -7,7 +7,7 @@ import androidx.room.Entity
  * Data Transfer Object to transfer tasks and task data from Retrofit to MVM
  */
 @Entity(tableName="tasks")
-data class Activity (var activityId : String = "",
+data class Activity (var activityId : Int = 0,
                      var activityName : String = "",
                      //Might need updated to string when implement drop down box for this choice
                      var isCompleted : Boolean = false) {
