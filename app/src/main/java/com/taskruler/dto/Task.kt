8f272debd476b76c2a50625672b2e46350f61224
wryther <rytherwy@mail.uc.edu>
2023-03-26 00:12:09 -0400

    Changed toString and isCompleted value

--- app/src/main/java/com/taskruler/dto/Task.kt
+++ app/src/main/java/com/taskruler/dto/Task.kt
@@ -9,9 +9,11 @@ import androidx.room.Entity
 @Entity(tableName="tasks")
 data class Task (var taskId : String = "",
                  var taskName : String = "",
-                 var isCompleted : Boolean = false ) {
+                 //Might need updated back to boolean when implement drop down box for this choice
+                 //var isCompleted : Boolean = false
+                 var isCompleted : String = "" ) {
     override fun toString(): String {
-        return taskName
+        return "$taskName"
     }
 }
 
