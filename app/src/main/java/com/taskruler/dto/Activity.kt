a785b1103a5c00d631febc2f9326c1ed61339edb
wryther <rytherwy@mail.uc.edu>
2023-03-26 00:41:58 -0400

    Renamed to activity, switched isCompleted back to bool

--- app/src/main/java/com/taskruler/dto/Activity.kt
+++ app/src/main/java/com/taskruler/dto/Activity.kt
@@ -7,13 +7,13 @@ import androidx.room.Entity
  * Data Transfer Object to transfer tasks and task data from Retrofit to MVM
  */
 @Entity(tableName="tasks")
-data class Task (var taskId : String = "",
-                 var taskName : String = "",
-                 //Might need updated back to boolean when implement drop down box for this choice
-                 //var isCompleted : Boolean = false
-                 var isCompleted : String = "" ) {
+data class Activity (var activityId : String = "",
+                     var activityName : String = "",
+                     //Might need updated to string when implement drop down box for this choice
+                     var isCompleted : Boolean = false) {
+                     //var isCompleted : String = ""
     override fun toString(): String {
-        return "$taskName"
+        return "$activityName"
     }
 }
 
