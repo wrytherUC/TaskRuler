0f35c5b9d46f92aa7a1c740353c85830c5a9fbf9
wryther <rytherwy@mail.uc.edu>
2023-03-26 02:02:49 -0400

    Added override for toString

--- app/src/main/java/com/taskruler/dto/UserTask.kt
+++ app/src/main/java/com/taskruler/dto/UserTask.kt
@@ -3,10 +3,15 @@ package com.taskruler.dto
 import androidx.room.Entity
 
 @Entity(tableName="UserTasks")
-data class UserTask (var userTaskID : Int,
-                     var userTaskName : String,
-                     var totalUserTaskTime : Int) {
-
+data class UserTask (var activityId: Int = 0,
+                     var activityName: String = "",
+                     var userTaskId : String = "",
+                     var userTaskName : String = "",
+                     var userIsCompleted : String = "",
+                     var userTotalTaskTime : Int = 0) {
+    override fun toString(): String {
+        return "$activityName"
+    }
 }
 
 //Example
