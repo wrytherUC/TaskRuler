7ec10b5698255c3a6d165b5d888b619496e810eb
wryther <rytherwy@mail.uc.edu>
2023-04-13 21:51:36 -0400

    Updated for new icons

--- app/src/main/java/com/taskruler/utilities/NotificationHelper.kt
+++ app/src/main/java/com/taskruler/utilities/NotificationHelper.kt
@@ -5,10 +5,12 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.taskruler.MainActivity
+import com.taskruler.R
 
 class NotificationHelper(val context: Context) {
     private val CHANNEL_ID = "task_reminder_channel_id"
@@ -48,17 +50,17 @@ class NotificationHelper(val context: Context) {
 
         //val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
-        //val icon = BitmapFactory.decodeResource(context.resources, R.drawable.reminder_char)
+        val icon = BitmapFactory.decodeResource(context.resources, R.drawable.reminder_char)
 
         //Might update style and otherwise tune this builder at a later time
         val notification = NotificationCompat.Builder(context, CHANNEL_ID)
-            //.setSmallIcon(R.drawable.checklist)
-            //.setLargeIcon(icon)
+            .setSmallIcon(R.drawable.checklist)
+            .setLargeIcon(icon)
             .setContentTitle(title)
             .setContentText(message)
-            //.setStyle(
-                //NotificationCompat.BigPictureStyle().bigPicture(icon).bigLargeIcon(null)
-            //)
+            .setStyle(
+                NotificationCompat.BigPictureStyle().bigPicture(icon).bigLargeIcon(null)
+            )
             .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .build()
