4da10d06332e0baaf35537e1eaf1688dfa392b5f
wryther <rytherwy@mail.uc.edu>
2023-04-20 03:02:04 -0400

    Edits from IDE suggestions

--- app/src/main/java/com/taskruler/TimerMainViewModel.kt
+++ app/src/main/java/com/taskruler/TimerMainViewModel.kt
@@ -4,7 +4,6 @@ import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.taskruler.dto.Activity
 import com.taskruler.helper.SingleLiveEvent
 import com.taskruler.utilities.TimerUtility.formatTime
 import com.taskruler.view.TIME_COUNTDOWN
@@ -36,7 +35,7 @@ class TimerMainViewModel : ViewModel() {
     fun handleCountDownTimer() {
         if (isPlaying.value == true) {
             pauseTimer()
-        } else if (_firstRun == true) {
+        } else if (_firstRun) {
             startTimer()
         }
         else {
@@ -51,7 +50,7 @@ class TimerMainViewModel : ViewModel() {
         handleTimerValues(false, time.value!!, globalProgress.value!!)
     }
 
-    fun startTimer() {
+    private fun startTimer() {
         _firstRun = false
         _isPlaying.value = true
         countDownTimer = object : CountDownTimer(TIME_COUNTDOWN, 1000) {
