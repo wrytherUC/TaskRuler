fad87ef54f6185a19d40b9c0d141334fc4b7732b
wryther <rytherwy@mail.uc.edu>
2023-04-20 02:58:08 -0400

    Moved time countdown to screen kt to pass value to it

--- app/src/main/java/com/taskruler/utilities/TimerUtility.kt
+++ app/src/main/java/com/taskruler/utilities/TimerUtility.kt
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit
 
 object TimerUtility {
     //time to countdown - 1min, this val is what is determining the countdown
-    var TIME_COUNTDOWN: Long = 60000L
     private const val TIME_FORMAT = "%02d:%02d"
 
     //convert time to milli seconds
