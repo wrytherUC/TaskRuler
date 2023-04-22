2a0bfbf5ab377863317c7f9b7d3e75779c9b79ea
wryther <rytherwy@mail.uc.edu>
2023-04-22 06:01:48 -0400

    Removed Toast import

--- app/src/main/java/com/taskruler/MainViewModel.kt
+++ app/src/main/java/com/taskruler/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.taskruler
 
 import android.util.Log
-import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
