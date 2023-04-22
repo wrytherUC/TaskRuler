c88622d35bf1c69e4f6df59e5b6ddb63eecd4156
wryther <rytherwy@mail.uc.edu>
2023-04-22 05:27:31 -0400

    Updated code based on LaEace Peyton code review

--- app/src/main/java/com/taskruler/RetrofitClientInstance.kt
+++ app/src/main/java/com/taskruler/RetrofitClientInstance.kt
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory
  */
 object RetrofitClientInstance {
     private var retrofit: Retrofit? = null
-    private val BASE_URL = "https://wrytheruc.github.io/"
+    private const val BASE_URL = "https://wrytheruc.github.io/"
 
     val retrofitInstance : Retrofit?
         get() {
